2951b4824d3a2392c741c7761481be3ef14a4034
Jessica Yeh <jessicayeh@fb.com>
2017-06-21 13:10:35 -0700

    add gitignore

---
+++ TumblrFeed/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  TumblrFeed
+//
+//  Created by Jessica Yeh on 6/21/17.
+//  Copyright © 2017 Jessica Yeh. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
